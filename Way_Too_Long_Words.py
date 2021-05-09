9b04804d64ece23d2ce548ec3ce694395e8d1291
thanhtunghoang <43109425+thanhtunghoang@users.noreply.github.com>
2021-05-09 12:25:32 +0700

    Add files via upload

---
+++ Way_Too_Long_Words.py
@@ -0,0 +1,12 @@
+i = input()
+a = len(i)
+if a >= 10:
+    # print("10 ky tu")
+    print(i[0], end = '')
+    print(a, end = '')
+    print(i[-1])
+# elif(a > 1):
+#     print(i)
+else:
+    pass
+# print(a)
